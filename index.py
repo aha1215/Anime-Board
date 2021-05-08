fee5e4481e9c081544853eb20c617732a91f32a2
ahernandez1215 <ahernandez@protonmail.com>
2021-05-08 15:38:46 -0700

    added search by keyword feature

--- index.py
+++ index.py
@@ -7,7 +7,8 @@ Flask app engine for Anime Board project.
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from PIL import Image
-import random
+import random, requests, json
+from pprint import pprint
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -27,6 +28,28 @@ TODO on all routes:
 def index():
     return render_template('index.html')
 
+#Search Results route
+@app.route('/search', methods=['GET','POST'])
+def search():
+    if request.method == 'POST':
+        search_term = request.form['keywords']
+        payload = {
+            'q': search_term
+        }
+        endpoint = 'https://api.jikan.moe/v3/search/anime'
+
+        r = requests.get(endpoint, params=payload)
+        data = r.json()
+        
+        my_list = []
+
+        for i in range(len(data)):
+            my_list.append(data['results'][i]['image_url']) #List of images matching search_term
+
+        return render_template('search_results.html', images=my_list, keywords=search_term)
+    
+    return render_template('search_results.html')
+
 #Image page route
 @app.route('/img/<img_id>')
 def imgpage():
@@ -36,11 +59,11 @@ def imgpage():
     #Send img info and comment info with render_template
     return render_template('imgpage.html')
 
-#Comment page route
+#Comment page route: 
 #TODO: This shouldn't run unless user is logged-in, redirect to login page instead.
 @app.route('/img/<img_id>/comment')
 def comment():
-    return render_template('newPost.html')
+    return render_template('comments.html')
     
 
 #Random image board route
@@ -50,20 +73,18 @@ def random():
     #Send images with render_template
     return render_template('random.html')
 
-#New post GET route
-@app.route('/new/post')
-def create_post():
-    #Get user input and store in database under posts table
-    return render_template('newPost.html')
-
 #New post POST route
 #Referenced https://flask.palletsprojects.com/en/1.1.x/quickstart/#accessing-request-data
-@app.route('/new/post', methods=['POST'])
+@app.route('/new/post', methods=['GET','POST'])
 def create_post_post():
-    description = request.form['description']
-    #Testing if data is sent to form...result ..yes
-    #Get user input and store in database under posts table
-    return render_template('newPost.html', info=description)
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        #Store user id + postId + postTitle,
+        return render_template('new_post.html', info=description, title=title)
+    else:
+        return render_template('new_post.html')
+
 
 #Login route
 @app.route('/login')
